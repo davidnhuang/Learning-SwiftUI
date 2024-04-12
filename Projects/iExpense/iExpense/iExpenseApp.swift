a18475faa4342861e6be31cf07599b58f1a6de6a
FrameDavidHuang <dhuang@frame.io>
2024-04-12 09:38:02 -0700

    Starting iExpense project

---
+++ Projects/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by David Huang (Work) on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
