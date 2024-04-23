a9cc6c4cd5895c5ec866d614e7bd9e0c61a1947c
David <david.n.huang@gmail.com>
2024-04-23 16:24:25 -0700

    Continue iExpense Project

--- Projects/iExpense/iExpense/ContentView.swift
+++ Projects/iExpense/iExpense/ContentView.swift
@@ -7,10 +7,46 @@
 
 import SwiftUI
 
+// structure of a single expense item
+// for it to conform to identifiable, you must have an id component to the struct
+// this also means we don't have to have the id:\.id in our forEach closure
+struct ExpenseItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    // class that stores many expense items in an array
+    var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
     
+    @State private var expenses = Expenses()
+    
     var body: some View {
-        Text("Hi")
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItem)
+            }
+            .navigationTitle("Expenses")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    let expense = ExpenseItem(name: "Text", type: "Personal", amount: 2)
+                    expenses.items.append(expense)
+                }
+            }
+        }
+    }
+    
+    func removeItem(at offset: IndexSet) {
+        expenses.items.remove(atOffsets: offset)
     }
     
 }
