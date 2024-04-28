e39a8d5c6bb458ab7200323bdb0d67bb8bd2ccbb
David <david.n.huang@gmail.com>
2024-04-28 10:53:50 -0700

    Completed iExpense project
    Lots more to learn - the whole section about UserDefaults and Class is confusing

---
+++ Projects/iExpense/iExpense/AddView.swift
@@ -0,0 +1,48 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by David Huang on 4/24/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    // listens for any dismiss events - this is how you dismiss sheet programmatically
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    var expenses: Expenses
+    
+    let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(types, id:\.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add New Expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
--- Projects/iExpense/iExpense/ContentView.swift
+++ Projects/iExpense/iExpense/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 // structure of a single expense item
 // for it to conform to identifiable, you must have an id component to the struct
 // this also means we don't have to have the id:\.id in our forEach closure
-struct ExpenseItem: Identifiable {
+struct ExpenseItem: Identifiable, Codable {
     let id = UUID()
     let name: String
     let type: String
@@ -20,28 +20,55 @@ struct ExpenseItem: Identifiable {
 @Observable
 class Expenses {
     // class that stores many expense items in an array
-    var items = [ExpenseItem]()
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.setValue(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+    }
 }
 
 struct ContentView: View {
     
     @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
     
     var body: some View {
         NavigationStack {
             List {
                 ForEach(expenses.items) { item in
-                    Text(item.name)
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.body)
+                            Text(item.type)
+                                .font(.caption)
+                        }
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
                 }
                 .onDelete(perform: removeItem)
             }
             .navigationTitle("Expenses")
             .toolbar {
                 Button("Add Expense", systemImage: "plus") {
-                    let expense = ExpenseItem(name: "Text", type: "Personal", amount: 2)
-                    expenses.items.append(expense)
+                    showingAddExpense = true
                 }
             }
+            .sheet(isPresented: $showingAddExpense, content: {
+                AddView(expenses: expenses)
+            })
         }
     }
     
