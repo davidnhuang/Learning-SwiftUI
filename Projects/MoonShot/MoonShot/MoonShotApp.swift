3c3d88d0d70b6e4bc3253344e36d215bc46ca3e9
David <david.n.huang@gmail.com>
2024-09-10 17:48:40 -0700

    I'm Back 🕶️

---
+++ Projects/MoonShot/MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by David Huang on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
